5210addc70d896c1dbac2a7e4bfac8c28521e217
Julian Gutierrez <julian.3.1@gmail.com>
2014-11-12 23:37:28 -0300

    Ejercicio 1

--- src/test/java/ar/edu/itba/it/paw/Start.java
+++ src/test/java/ar/edu/itba/it/paw/Start.java
@@ -11,7 +11,7 @@ public class Start {
 		SocketConnector connector = new SocketConnector();
 		connector.setMaxIdleTime(1000 * 60 * 60);
 		connector.setSoLingerTime(-1);
-		connector.setPort(8080);
+		connector.setPort(8090);
 		server.setConnectors(new Connector[] { connector });
 		WebAppContext bb = new WebAppContext();
 		bb.setServer(server);
